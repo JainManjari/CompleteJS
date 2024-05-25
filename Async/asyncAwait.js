f84d716ff5f82352450a2257cfa73fa7daf85fad
Manjari Jain <manjjain@paypal.com>
2024-05-25 15:37:31 +0530

    comparison btw promise and async await

--- Async/asyncAwait.js
+++ Async/asyncAwait.js
@@ -21,8 +21,6 @@ function goToHomePage(userAuthStatus) {
   return Promise.resolve(`Go to home page as ${userAuthStatus}`);
 }
 
-
-
 // chaining of promises
 // userLogin()
 //   .then((response) => {
@@ -35,18 +33,40 @@ function goToHomePage(userAuthStatus) {
 //     console.log("err ", err);
 //   });
 
+// async function asyncAwaitUserLogin () {
+//   try {
+//       let response = await userLogin();
+//       console.log("response ", response);
+//       let userAuthStatus = await goToHomePage("userAuthorised");
+//       console.log("userAuthStatus ", userAuthStatus);
+//   } catch(err) {
+//     console.log("Err ", err);
+//   }
+// }
 
+// asyncAwaitUserLogin();
 
-
-  async function asyncAwaitUserLogin () {
-    try {
-        let response = await userLogin();
-        console.log("response ", response);
-        let userAuthStatus = await goToHomePage("userAuthorised");
-        console.log("userAuthStatus ", userAuthStatus);
-    } catch(err) {
-      console.log("Err ", err);
-    }
+const makeAPICall = (milliseconds) => {
+  return () => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log("API call completed in ", milliseconds);
+      }, milliseconds);
+    });
   }
+};
 
-  asyncAwaitUserLogin();
+const APICalls = [makeAPICall(1000), makeAPICall(500), makeAPICall(2500)];
+
+// Promise.all(APICalls).then((values) => {
+//   console.log("values"); // all the api calls happen parallely
+// });
+
+
+
+// it will wait till the response of one api, then process with the next api call 
+(async function () {
+  for (let api of APICalls) {
+    console.log(await api());
+  }
+})();
