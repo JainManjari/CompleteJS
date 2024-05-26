d743c713392843b48d24fc98ee3d814c625febdf
Manjari Jain <manjjain@paypal.com>
2024-05-26 13:56:41 +0530

    learning redux and created actions

---
+++ Redux/redux.js
@@ -0,0 +1,23 @@
+// Action creators
+
+// person who is booking the ticket
+
+const newBooking = (name, amount) => {
+  return {
+    type: "NEW_BOOKING",
+    payload: {
+      name,
+      amount,
+    },
+  };
+};
+
+const cancelBooking = (name, refundAmount) => {
+  return {
+    type: "CANCEL_BOOKING",
+    payload: {
+      name,
+      refundAmount,
+    },
+  };
+};
